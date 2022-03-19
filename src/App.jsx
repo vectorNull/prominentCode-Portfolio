be36df9e10dc96397be5684e250a5e36c9f46d15
Abdul Ghaffar Rodriguez <abdul_ghaffar@prominentcode.com>
2022-03-19 13:00:29 -0700

    Boilerplate setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import { About, Footer, Header, Skills, Testimonials, Work } from "./containers";
+import { Navbar } from "./components";
+import './App.scss'
+
 const App = () => {
-	return <div>App</div>;
+	return <div className="app">
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonials />
+        <Footer />
+        </div>;
 };
 
 export default App;
